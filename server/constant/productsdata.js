0f079531bace711c68af867ef802c771847420d6
Ashutosh Singh <113412956+ashutosh-singh83@users.noreply.github.com>
2024-10-05 19:47:44 +0530

    Initial commit

---
+++ server/constant/productsdata.js
@@ -0,0 +1,89 @@
+const products = [
+  { 
+      id:"products1",
+      url: 'https://d1tnljjgyfxvtr.cloudfront.net/uploads/32dd3844-6d35-4cde-9611-5920f55a7e52.jpeg', 
+      detailUrl: 'https://d1tnljjgyfxvtr.cloudfront.net/uploads/32dd3844-6d35-4cde-9611-5920f55a7e52.jpeg',
+      title: {
+          shortTitle: 'Home & Kitchen',
+          longTitle: 'Mango Ginger Pickle With Sesame Oil'
+      }, 
+      price: {
+          mrp: 510,
+          cost: 210,
+          discount: '47%'
+      },
+      description: 'Course - Pickel Serving temperature - Room temperature Main ingredients - Dried Mango, Ginger, Sesami oil, powdered Mirchi, Jagerry',
+      discount: 'Extra 10% Off', 
+      tagline: 'Deal of the day' 
+  },
+  { 
+      id:"products2",
+      url: 'https://d1tnljjgyfxvtr.cloudfront.net/uploads/336e6ada-eeef-4a11-87f3-6dbda906bb5f.jpeg', 
+      detailUrl: 'https://d1tnljjgyfxvtr.cloudfront.net/uploads/336e6ada-eeef-4a11-87f3-6dbda906bb5f.jpeg',
+      title: {
+          shortTitle: 'Kajjikayalu',
+          longTitle: 'Category: Village Flavours'
+      },
+      price: {
+          mrp: 565,
+          cost: 385,
+          discount: '40%'
+      },
+      description: 'stuffed with Coconut louz *Andra vantalu',
+      discount: 'From 99+5% Off', 
+      tagline: 'Pestige, Nova & more' 
+  },
+  { 
+       id:"products3",
+      url: 'https://d1tnljjgyfxvtr.cloudfront.net/uploads/154dd750-3238-4f81-8178-83b241f03150.jpeg', 
+      detailUrl: 'https://d1tnljjgyfxvtr.cloudfront.net/uploads/154dd750-3238-4f81-8178-83b241f03150.jpeg',
+      title: {
+          shortTitle: 'Dry Dates',
+          longTitle: 'Category: Dry Fruits'
+      }, 
+      price: {
+          mrp: 315,
+          cost: 215,
+          discount: '30%'
+      },
+      description: ' High in Fiber. Getting enough fiber is important for your overall health. ... . High in Disease-Fighting Antioxidants. ... . May Promote Brain Health. ... . May Promote Natural Labor. ... . Excellent Natural Sweetener. ... . Other Potential Health Benefits. ... . Easy to Add to Your Diet.',
+      discount: 'Upto 70% Off', 
+      tagline: 'Deal of the Day' 
+  },
+  { 
+       id:"products4",
+      url: 'https://d1tnljjgyfxvtr.cloudfront.net/uploads/b6dd7205-5f55-42c2-8b6c-9eded34a16db.jpeg', 
+      detailUrl: 'https://d1tnljjgyfxvtr.cloudfront.net/uploads/b6dd7205-5f55-42c2-8b6c-9eded34a16db.jpeg',
+      title: {
+          shortTitle: 'Cassia bark',
+          longTitle: 'Category: Indian Spices',
+      }, 
+      price: {
+          mrp: 320,
+          cost: 215,
+          discount: '32%'
+      },
+      description: 'Cassia bark is an interesting spice. Also known as Chinese cinnamon, it is a genus of the cinnamon tree. Cinnamon is a little bit different from cassia, and usually differentiated by being called “true cinnamon.” Cassia is cheaper to produce, and the majority of ground cinnamon is actually made from cassia bark. Indians use cassia instead of true cinnamon in their cooking, as it has a milder flavor and can be used in larger quantities.',
+      discount: 'Grab Now', 
+      tagline: 'Best Seller' 
+  },
+  { 
+       id:"products5",
+      url: 'https://d1tnljjgyfxvtr.cloudfront.net/uploads/6ed53c0a-bf34-4826-892e-3c8ea93bb9c4.jpeg',
+      detailUrl: 'https://d1tnljjgyfxvtr.cloudfront.net/uploads/6ed53c0a-bf34-4826-892e-3c8ea93bb9c4.jpeg', 
+      title: {
+          shortTitle: 'Chandan Powder',
+          longTitle: 'Category: Pooja Items'
+      }, 
+      price: {
+          mrp: 478,
+          cost: 278,
+          discount: '45%'
+      },
+      description: 'Chandanam, scientific name ‘Santalum album’ is known for its therapeutic properties and is used extensively in natural treatments. Sandalwood is also known as santal oil, white saunders oil, white or yellow sandalwood oil, East Indian sandalwood oil, chandan, Indian Sandalwood, etc.',
+      discount: 'From ₹278', 
+      tagline: 'Kubra, Nova & more' 
+  }
+];
+
+module.exports = products;
